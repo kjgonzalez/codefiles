56e9c60dda26bcf36de4b186326e5cc8dfd9810f
kris <kg-171971@hs-weingarten.de>
2018-11-17 16:42:04 +0100

    corrected pin number

--- cpp_sandbox/arduino_all/_140512_Serial_Input/_140512_Serial_Input.ino
+++ cpp_sandbox/arduino_all/_140512_Serial_Input/_140512_Serial_Input.ino
@@ -4,7 +4,7 @@ command to arduino from serial monitor.
 should be able to write something like "1"
 and have the LED blink once.
 
-UDPATE: it works. 
+UDPATE: it works.
 */
 
 int n=0;
@@ -14,7 +14,7 @@ void setup()
 {
 
 	Serial.begin(9600);
-	pinMode(12,OUTPUT);
+	pinMode(13,OUTPUT);
 	delay(5000);
 	Serial.println("starting. lol ..");
 
@@ -28,12 +28,12 @@ void loop(){
 		Serial.println(n);
 
 			n=constrain(n,1,10);
-			
+
 			for(int i=0;i<n;i++){
-				digitalWrite(12,HIGH);
+				digitalWrite(13,HIGH);
 				Serial.println("high");
 				delay(300);
-				digitalWrite(12,LOW);
+				digitalWrite(13,LOW);
 				Serial.println("low");
 				delay(300);
 			} //blink forloop
