85a406db9c7d56e0d8b48ff1493f463de7f4691e
kris <kg-171971@hs-weingarten.de>
2018-11-17 17:24:23 +0100

    added further instructions

--- cpp_sandbox/arduino_all/_181117_echo_test/echo_test_pc.py
+++ cpp_sandbox/arduino_all/_181117_echo_test/echo_test_pc.py
@@ -9,20 +9,30 @@ kjgnote: this is EXTREMELY simple way of receiving data. not recommended,
     to correctly transmit and receive data.
 
 How to run this script:
->> python3 echo_test_pc.py
+>> python3 echo_test_pc.py SERIAL_PORT
+
+SERIAL_PORT: user given name of port where board is connected
+examples: '/dev/ttyACM2', 'COM0'
+# NOTE: in windows, this may be COM*. linux: /dev/tty*
+
+possible ways to list ubuntu serial ports: 
+>> dmesg | grep tty
+>> ls /dev/tty* | grep AC
+
 '''
 
 import serial
 import time
+from sys import argv
+
 ser = serial.Serial()
 ser.baudrate = 9600 # very slow
 # ser.timeout = 0.1 # amount of time to wait for new data
-ser.port = '/dev/ttyACM2'
-# NOTE: in windows, this may be COM*. linux: /dev/tty*
+ser.port = argv[1]
 
 ser.open()
 inp=''
-
+print('Connection established. Press "q" to exit')
 while(inp != 'q'):
     inp = input('PC: ')
     if(inp!='q'):
