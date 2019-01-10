163f0d50a2cca9d665f74e83d788382de7d0a9d4
kris <kjg9190@hotmail.com>
2019-01-10 14:34:55 +0100

    creating quick setup help

---
+++ setup_help/k_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install \
+terminator \
+chromium-browser \
+python3-pip \
+git \
+kate \
+pinta \
+vim\
+compizconfig-settings-manager
+sudo pip3 install --upgrade pip # after 1st run, just pip
+pip install -r python_requirements.txt --user
+
