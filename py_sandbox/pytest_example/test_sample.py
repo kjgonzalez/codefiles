d8a40fef7cd401790ffcbc38e779ba376d967401
kris <kjg9190@hotmail.com>
2019-01-14 22:45:47 +0100

    local pytest example

---
+++ py_sandbox/pytest_example/test_sample.py
@@ -0,0 +1,22 @@
+'''
+Objective: this file serves as the test module for the other library module, and
+    will thus import the other module to be used here.
+
+See module_lib.py for more information
+'''
+
+import module_lib as l
+
+
+
+def test_pyt():
+    assert l.pyt([3,4,12]) == 13
+
+def test_pyt2():
+    assert l.pyt([4,4]) == 4*2**0.5
+
+def test_area():
+    assert l.area_rect(-10,-10) == 100
+
+def test_area2():
+    assert l.area_rect(34,10) == 340
