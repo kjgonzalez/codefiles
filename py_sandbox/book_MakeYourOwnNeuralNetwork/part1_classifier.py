d4635874364ef98d19c2936a8362ad97b20c9c16
kris <kjg9190@hotmail.com>
2019-01-22 23:59:26 +0100

    more on part1

--- py_sandbox/book_MakeYourOwnNeuralNetwork/part1_classifier.py
+++ py_sandbox/book_MakeYourOwnNeuralNetwork/part1_classifier.py
@@ -22,19 +22,27 @@ def genPts(n=10,center=(0,0),radius=1.0):
 caterpillars=genPts(center=(1,4))
 ladybirds=genPts(center=(4,1))
 
-# !-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-
-a=np.deg2rad(30) # this is the value we want to optimize
-# !-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-
+# learning parameters
+lr = 0.01
+# initialize angle of guess
+a=0.25 # will just express the angle as a slope
 
 length=5.5
+
+print('iter, estimate, error, d_a, new a')
+for i,ipair in enumerate(np.row_stack((caterpillars,ladybirds))):
+    # import ipdb; ipdb.set_trace()
+    # for each training example, modify variable 'a'
+    estimate=a*ipair[0]
+    err = estimate-ipair[1]
+    d_a=lr*(err/ipair[0])
+    a+=-d_a
+    print('Iter{}: {}, {:.2f}, {:.2f}, {:.2f}, {:.2f}'.format(i,np.round(ipair,2),estimate,err,d_a,a) )
+
+
 divline=np.array([[0,0],[length*np.cos(a),length*np.sin(a)]])
-
-
-
-
-
-
 plt.plot(caterpillars[:,0],caterpillars[:,1],'b.')
 plt.plot(ladybirds[:,0],ladybirds[:,1],'r.')
 plt.plot(divline[:,0],divline[:,1],'g-')
+plt.grid()
 plt.show()
