6496c681931f7b25a74b47742bf5193c6116c335
kris <kjg9190@hotmail.com>
2019-01-22 22:44:32 +0100

    misc

--- py_sandbox/book_MakeYourOwnNeuralNetwork/part1_classifier.py
+++ py_sandbox/book_MakeYourOwnNeuralNetwork/part1_classifier.py
@@ -1,5 +1,5 @@
 '''
-Objective: do a little bit of practice with the book "Make Your own Neural Network", by Tariq Rashid. basically, this code will be from the first part.
+Objective: do a little bit of practice with the book "Make Your Own Neural Network", by Tariq Rashid. basically, this code will be from the first part.
 '''
 
 # PAGE 19
@@ -14,7 +14,7 @@ def genPts(n=10,center=(0,0),radius=1.0):
     assert type(n) ==int,"n must be integer"
     assert len(center)==2,"center must be (x,y) coordinate tuple"
     assert float(radius)>0.0,"radius must be greater than zero"
-    
+
     x=center[0]+(np.random.rand(n)-0.5)*2*float(radius)
     y=center[1]+(np.random.rand(n)-0.5)*2*float(radius)
     return np.array([x,y]).transpose()
@@ -38,5 +38,3 @@ plt.plot(caterpillars[:,0],caterpillars[:,1],'b.')
 plt.plot(ladybirds[:,0],ladybirds[:,1],'r.')
 plt.plot(divline[:,0],divline[:,1],'g-')
 plt.show()
-
-
