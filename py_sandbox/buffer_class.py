c0839525edc44eb22af17693f017a7d80f76daf0
kris <kjg9190@hotmail.com>
2019-03-14 09:30:44 +0100

    slightly improved movement detection, created a buffer class

---
+++ py_sandbox/buffer_class.py
@@ -0,0 +1,58 @@
+'''
+Objective: create simple buffer object class that can hold a certain number of 
+    things in memory and dump out stuff that at's "end" of buffer. Essentially,
+    just want to keep the latest L number of things in memory, and be able to 
+    recall them in chronological order
+'''
+
+class Buffer:
+    ''' Hold L number of things in buffer until needed. return buffer in
+        chronological order, "FIFO" 
+    EXAMPLE:
+        buf=Buffer(4)
+        buf.toBuffer('a')
+        buf.toBuffer('b')
+        buf.toBuffer('c')
+        buf.toBuffer('d')
+        buf.toBuffer('e')
+        buf.getBuffer 
+        >> ['b','c','d','e']
+    '''
+    def __init__(self,length):
+        self._L=length
+        self._buf=[[i,None] for i in range(length)]
+        self._k = 0 # pointer to latest location in buffer
+    # def __init__
+    
+    def __len__(self):
+        ''' return length of buffer, not number of contents held '''
+        return self._L
+
+    def toBuffer(self,item):
+        ''' put something in buffer, update pointer '''
+        self._buf[self._k][1]=item
+        self._k+=1
+        if(self._k==self._L):
+            self._k=0 # reset pointer back
+        return 0 # status that all is ok
+    # def toBuffer
+    
+    @property
+    def getBuffer(self):
+        ''' Return values in buffer (oldest first), ignoring any indices that 
+            haven't been written to yet. 
+        '''
+        out=[self._buf[i][1] for i in range(self._k,self._L) if \
+            self._buf[i][1]!=None]+ \
+                [self._buf[i][1] for i in range(0,self._k)]
+        return out
+    # def buffer
+    
+    @property
+    def length(self):
+        ''' return length of buffer, not number of contents held '''
+        return self._L
+    # def length
+# class Buffer
+
+# eof
--- py_sandbox/capMovement/moveDetect.py
+++ py_sandbox/capMovement/moveDetect.py
@@ -1,9 +1,10 @@
 '''
 objective: capture a certain number of frames and keep in buffer, while also recording the first 5 seconds that something shows up.
 
+source: https://www.pyimagesearch.com/2015/05/25/basic-motion-detection-and-tracking-with-python-and-opencv/
 will break up into steps: 
-1. just open up webcam
-4. be able to detect movement
+1. just open up webcam - done
+4. be able to detect movement - done
 2. record 2 seconds (60 frames)
 3. be able to constantly put frames into buffer and throw out old frames
 5. be able to have buffer that then writes and saves 2 seconds before movement is detected along with next 2 seconds of movement.
@@ -18,21 +19,56 @@ import klib
 assert klib.PYVERSION == 3, "Please use python version 3"
 
 ##############################
-vidsrc=0
+vidsrc=1
 
 # main =========================================================================
 
 cap = cv2.VideoCapture(vidsrc)
 B2G=cv2.COLOR_BGR2GRAY
-fold=cv2.cvtColor(cap.read(),B2G) # older 
-fnew=copy.copy(fold)  
+cap_fps=cap.get(cv2.CAP_PROP_FPS)
+cap_ht=cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+def quickTransform(cv2_img):
+    B2G=cv2.COLOR_BGR2GRAY
+    x=cv2.cvtColor(cv2_img,B2G)
+    x=cv2.resize(x,(0,0),fx=0.5,fy=0.5)
+    x=cv2.GaussianBlur(x,(21,21),0)
+    return x
+
+
+for i in range(30):
+    # throw out first few frames of video
+    frame0=quickTransform(cap.read()[1])
+
 while(True):
-    fold=copy.copy(fnew)
     # Capture frame-by-frame
+    raw=cap.read()[1]
     
-    ret, fnew = cap.read()
+    frame = quickTransform(raw)
+    frameDelta=cv2.absdiff(frame0,frame)
+    print(frameDelta.max())
     
-    cv2.imshow('frame',fnew)
+    # frame-to-frame, if there's no change greater than 10, keep new frame as reference
+    if(frameDelta.max()<5):
+        frame0=frame
+    
+    if(frameDelta.max()<30):
+        # nothing there
+        text='empty'
+    else:
+        # something there
+        text='detected'
+
+    cv2.putText(raw, "Room Status: {}".format(text), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+    # not really needed, but will keep anyway
+    # thresh=cv2.threshold(frameDelta,25,255,cv2.THRESH_BINARY)[1]
+    # thresh = cv2.dilate(thresh, None, iterations=2)
+    
+    cv2.imshow('raw',raw)
+    cv2.imshow('delta',frameDelta)
+    cv2.imshow('thresh',thresh)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -40,3 +76,5 @@ while(True):
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
