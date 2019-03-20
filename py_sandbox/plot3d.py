966fb15a1a699190d8483b612b0f20f8509d0b62
kris-iki <kristian.gonzalez@hs-weingarten.de>
2019-03-20 16:38:53 +0100

    plotting

---
+++ py_sandbox/plot3d.py
@@ -0,0 +1,18 @@
+'''
+Objective: demonstrate how to plot in 3D
+DateCreated: 190320
+source: https://matplotlib.org/gallery/mplot3d/scatter3d.html
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D # only required to "register 3D projection"
+
+z=np.linspace(0,10)
+x=np.cos(z)
+y=np.sin(z)
+
+f=plt.figure() # these steps are an alternative to doing: f,p=plt.subplots(...)
+p=f.add_subplot(111,projection='3d')
+p.scatter(x,y,z,c=z) # "c=z" means color each point as a function of z value
+plt.show()
