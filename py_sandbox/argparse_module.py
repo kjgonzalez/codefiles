598e0957ea88b31815ee25e3b66d2d80e88961eb
kris <kjg9190@hotmail.com>
2019-03-21 00:05:25 +0100

    argparse more fleshed out

--- py_sandbox/argparse_module.py
+++ py_sandbox/argparse_module.py
@@ -6,18 +6,35 @@ want to learn about:
     * initialization
     * optional / required args
     * valid types
+
+valid arguments for add_argument:
+    dest: name of argument. if not used, default is stripped arg value
+    help: quick help text should user type python file.py --help
+    default: assumed value if not declared
+    type: primitive variable types
+    action: ('store_true', 'store_false', 'count')
+
+sources:
+https://docs.python.org/3.7/howto/argparse.html
+https://docs.python.org/3/library/argparse.html
+https://github.com/jwyang/faster-rcnn.pytorch/blob/master/trainval_net.py
 '''
 
 import argparse
 
-parser=argparse.ArgumentParser()
-parser.add_argument('--arg1',dest='int1',type=int)
-parser.add_argument('--arg2',dest='int2',type=int)
-parser.add_argument('--str1',type=str,default='nice day',required=False)
-args=parser.parse_args()
+p=argparse.ArgumentParser()
+p.add_argument('-a',dest='int1',type=int,help='first int')
+p.add_argument('-b',dest='int2',type=int,help='second int')
+p.add_argument('-s',type=str,default="it's a nice day",help='string to print')
+p.add_argument('-cuda',default=False,action='store_true',help='enable cuda')
+args=p.parse_args()
 
 print(args.int1+args.int2)
-print(args.str1)
+print(args.s)
+if(args.cuda):
+    print('using cuda')
+else:
+    print('cuda disabled')
 
 # eof
 
