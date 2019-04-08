5dde4365a0fae6e68af134a8f4e2145848f5aa1f
kris_cell <kjg9190@hotmail.com>
2019-04-08 19:25:21 +0200

    cpp on phone

---
+++ cpp_sandbox/hello_phone/hello_phone.cpp
@@ -0,0 +1,13 @@
+/*
+KJG180305: able to compile using command: >> g++ code.cpp
+KJG190408: able to compile on phone. install termux and also install clang:
+  pkg install clang
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "hello phone!" << endl;
+	return 0;
+}
