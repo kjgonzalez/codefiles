19693aec4f38875aa8ff15599ab408105f7252ac
kris-iki <kristian.gonzalez@hs-weingarten.de>
2019-04-30 12:25:33 +0200

    added not-yet-understood method

--- cpp_sandbox/structs/about_structs.cpp
+++ cpp_sandbox/structs/about_structs.cpp
@@ -42,8 +42,21 @@ Cat meow(7,40);
 printf("Cat info:\n");
 printAgeHeight(meow.age,meow.height);
 
+// METHOD3: different kind of constructor, not yet understood ==================
+struct tBox {
+  string  type;     // object type as car, pedestrian or cyclist,...
+  double   x1;      // left corner
+  double   y1;      // top corner
+  double   x2;      // right corner
+  double   y2;      // bottom corner
+  double   alpha;   // image orientation
+  tBox (string _type, double _x1,double _y1,
+        double _x2,double _y2,double _alpha):
+    type(_type),x1(_x1),y1(_y1),x2(_x2),y2(_y2),alpha(_alpha) {}
+};
+tBox a("car",0,1,2,3,0.5);
 
-
+printf("%s: %f, %f, %f, %f, %f\n",a.type.c_str(),a.x1,a.y1,a.x2,a.y2,a.alpha);
 
 return 0;
 }//main
