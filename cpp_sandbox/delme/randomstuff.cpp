593007f19187a968897cc2c502075e7d62f2ba73
kris-iki <kristian.gonzalez@hs-weingarten.de>
2019-05-02 10:19:42 +0200

    etc

--- cpp_sandbox/delme/randomstuff.cpp
+++ cpp_sandbox/delme/randomstuff.cpp
@@ -3,15 +3,24 @@ emulate what you saw in "evaluate_object_3d_offline.cpp" file
 */
 
 #include <iostream>
-#include <vector>
+// #include <vector>
+// #include <stdio.h>
 using namespace std;
 
 enum CLASSES{CAR=3, PEDESTRIAN=4, CYCLIST=5};
 const int NUM_CLASS = 3;
 
 int main(){
-    vector<bool> eval_image(NUM_CLASS,false);
-    for(int i=0;i<eval_image.size();i++) cout<<eval_image[i]<< ' ';
-    printf("\n");
+    // vector<bool> eval_image(NUM_CLASS,false);
+    // for(int i=0;i<eval_image.size();i++) cout<<eval_image[i]<< ' ';
+    // printf("\n");
+
+    system("echo test");
+    /* try making file pointers */
+    // FILE *f;
+    // FILE *f = fopen("delme.txt","w");
+    // printf("hello world\n");
+    // system("rm delme.txt");
+    // fclose(f);
     return 0;
 }
