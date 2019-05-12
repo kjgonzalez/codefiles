75243065ffa41c0dd5da73f75262ace1b45154b5
kris <kjg9190@hotmail.com>
2019-05-12 23:02:11 +0200

    misc

---
+++ py_sandbox/p2p_chat/part1.py
@@ -0,0 +1,45 @@
+'''
+want to build a simple p2p program that can allow two computers to "chat". can
+    perhaps build upon in the future. will start with part one of a two part
+    series, given by "howCode"
+
+sources:
+    part1: https://www.youtube.com/watch?v=DIPZoZheMTo
+    part2: https://www.youtube.com/watch?v=D0SLpD7JvZI
+    part3: https://www.youtube.com/watch?v=Rvfs6Xx3Kww
+
+"threads" necessary to handle multiple connections at once
+* want to create multiple threads to handle multiple clients at once.
+
+
+KJGNOTE: this should be run on the "server"
+'''
+
+
+
+import socket, threading, sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4, tcp
+
+sock.bind(('0.0.0.0',10000))
+
+sock.listen(1)
+connections = []
+
+def handler(c,a):
+    global connections # want global access
+    while True:
+        data = c.recv(1024) # 1024 bytes max
+        for connection in connections:
+            connection.send(bytes(data)) # can only send bytes
+        if not data:
+            connections.remove(c)
+            c.close()
+            break
+while True:
+    c,a = sock.accept()
+    cThread = thrading.Thread(target=handler,args=(c,a))
+    cThread.daemon = True # allows you to close program even if threads still running
+    cThrad.start()
+    connections.append(c)
+    print(connections)
---
+++ py_sandbox/webcam_flipping.py
@@ -0,0 +1,46 @@
+'''
+source:
+https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+'''
+import numpy as np
+import cv2
+from sys import argv
+import klib
+assert klib.PYVERSION == 3, "Please use python version 3"
+import argparse
+
+p=argparse.ArgumentParser()
+p.add_argument('--src',type=int,default=0,help='video source')
+# p.add_argument('-s',type=str,default="it's a nice day",help='string to print')
+p.add_argument('--hFlip',default=False,action='store_true',help='flip img about horizontal')
+p.add_argument('--vFlip',default=False,action='store_true',help='flip img about vertical')
+args=p.parse_args()
+
+vidsrc=args.src
+
+cap = cv2.VideoCapture(vidsrc)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if(args.vFlip):
+        # flip about vertical:
+        frame=np.flip(frame,0)
+
+    if(args.hFlip):
+        # flip about vertical:
+        frame=np.flip(frame,1)
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    #cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
