6ec945f6e2c5aa3f5e62532bdad622eb14d9ef19
kris <kjg9190@hotmail.com>
2019-05-12 23:40:04 +0200

    shift to OOP style

--- py_sandbox/p2p_chat/part2.py
+++ py_sandbox/p2p_chat/part2.py
@@ -1,30 +1,35 @@
 '''
 part 2 of ultimately building a p2p chat program. see part1.py for all details.
+    Also next, will start working in a more object-oriented way.
+
 '''
 
 import socket, threading, sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4, tcp
+class Server:
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # ipv4, tcp
+    self.connections = []
+    def __init__(self):
+        self.sock.bind(('0.0.0.0',10000))
+        self.sock.listen(1)
+    def handler(self,c,a):
+        while True:
+            data = c.recv(1024) # 1024 bytes max
+            for connection in self.connections:
+                connection.send(bytes(data)) # can only send bytes
+            if not data:
+                self.connections.remove(c)
+                c.close()
+                break
+    def run(self):
+        while True:
+            c,a = self.sock.accept()
+            cThread = threading.Thread(target=handler,args=(c,a))
+            # allows you to close program even if threads still running
+            cThread.daemon = True
+            cThread.start()
+            self.connections.append(c)
+            print(self.connections)
 
-sock.bind(('0.0.0.0',10000))
-
-sock.listen(1)
-connections = []
-
-def handler(c,a):
-    global connections # want global access
-    while True:
-        data = c.recv(1024) # 1024 bytes max
-        for connection in connections:
-            connection.send(bytes(data)) # can only send bytes
-        if not data:
-            connections.remove(c)
-            c.close()
-            break
-while True:
-    c,a = sock.accept()
-    cThread = threading.Thread(target=handler,args=(c,a))
-    cThread.daemon = True # allows you to close program even if threads still running
-    cThread.start()
-    connections.append(c)
-    print(connections)
+server = Server()
+server.run()
