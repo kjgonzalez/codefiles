accf22bd7bd495423aae741a465bacfa06b53260
kris_cell <kjg9190@hotmail.com>
2019-05-29 09:37:18 +0200

    even simpler way with map function

--- py_sandbox/match.py
+++ py_sandbox/match.py
@@ -1,5 +1,8 @@
 '''
-simple function that helps match a value in an array to a given value.
+simple function that helps match a value in an array to a given value. example 
+    task is to compare a detection with several ground truth bounding boxes to
+    generate highest iou pair.
+kjg190529: have discovered even simpler way of finding best match. described below.
 '''
 
 import numpy as np
@@ -17,6 +20,7 @@ def find_best_match(value,array,function,objective='min'):
         return np.argmax(results)
 
 print('example 1 '+'-'*30)
+# here, want to find index of value closest to a, 2.0
 a=2.0
 b=[0.0,1.0,3.5,2.1,2.5,3.0]
 def f(a,b):
@@ -26,9 +30,22 @@ print('ind {}: {}'.format(ind,b[ind]))
 
 # ==============================================================================
 print('example 2 '+'-'*30)
+# here, give index of value that produces highest result.
 a=2.0
 b=[0.0,1.0,3.5,2.1,2.5,3.0]
 def f2(a,b):
     return a*b
 ind=find_best_match(a,b,f2,objective='max')
 print('ind {}: {}'.format(ind,b[ind]))
+
+# ==============================================================================
+print('example 3'+'-'*30)
+# ok, try to do it in one line with argmin / argmax
+a=2.0
+b=[0.0,1.0,3.5,2.1,2.5,3.0]
+def f3(ival):
+    return a*ival
+ind = np.argmax(list(map(f3,b)))
+print('ind {}: {}'.format(ind,b[ind]))
+
+
