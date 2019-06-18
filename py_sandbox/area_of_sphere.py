d48dad212ce78438fe15a663a43f08ff32c70feb
kris-iki <kristian.gonzalez@hs-weingarten.de>
2019-06-18 10:30:09 +0200

    debugging

--- py_sandbox/area_of_sphere.py
+++ py_sandbox/area_of_sphere.py
@@ -9,11 +9,22 @@ $$ A_{sphere}=R^2\theta (\sin{b} - \sin{a})$$
 import numpy as np
 
 def AreaSpherePartial(radius=1,horizAngle=2*np.pi,
-            vertAngle1=-np.pi/2,vertAngle2=np.pi/2):
+            vertAngle1=-np.pi/2,vertAngle2=np.pi/2,useDeg=False):
     '''
-    Return partial area of sphere, given radius, hAngle,vAngle start & finish
+    Return partial area of sphere, given parameters.
+    INPUT:
+        * radius: radius of sphere. default=1
+        * horizAngle: 
+        * vertAngle1:
+        * vertAngle2:
+    radius, hAngle,vAngle start & finish
         (radians)
     '''
+    if(useDeg):
+        horizAngle*=np.pi()/180
+        vertAngle1*=np.pi()/180
+        vertAngle2*=np.pi()/180
+
     return radius**2 * horizAngle * (np.sin(vertAngle2) - np.sin(vertAngle1))
 
 if(__name__=='__main__'):
