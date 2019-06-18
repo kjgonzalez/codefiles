93f554a0e643d04fa5d4e5f0acd192244a089bde
kris_cell <kjg9190@hotmail.com>
2019-06-18 10:45:48 +0200

    usable with degrees now

--- py_sandbox/area_of_sphere.py
+++ py_sandbox/area_of_sphere.py
@@ -14,16 +14,17 @@ def AreaSpherePartial(radius=1,horizAngle=2*np.pi,
     Return partial area of sphere, given parameters.
     INPUT:
         * radius: radius of sphere. default=1
-        * horizAngle: 
-        * vertAngle1:
-        * vertAngle2:
-    radius, hAngle,vAngle start & finish
-        (radians)
+        * horizAngle: horizontal FOV / angle. default=2*pi.
+        * vertAngle1: vertical FOV / angle start point. default=-pi/2
+        * vertAngle2: vert FOV/angle end point. default=pi/2
+        * useDeg: boolean. enable degrees. default=False
+    OUTPUT:
+        * area: scalar.
     '''
     if(useDeg):
-        horizAngle*=np.pi()/180
-        vertAngle1*=np.pi()/180
-        vertAngle2*=np.pi()/180
+        horizAngle*=np.pi/180
+        vertAngle1*=np.pi/180
+        vertAngle2*=np.pi/180
 
     return radius**2 * horizAngle * (np.sin(vertAngle2) - np.sin(vertAngle1))
 
@@ -31,3 +32,8 @@ if(__name__=='__main__'):
     params = (1,2*np.pi,-np.pi/2,np.pi/2)
     print('parameters:',params)
     print(AreaSpherePartial(*params))
+    params = (1,360,-90,90,True) # use degrees
+    print('with deg:',params)
+    print(AreaSpherePartial(*params))
+
+
