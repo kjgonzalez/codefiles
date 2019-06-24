44ebf86a6a657d23b8969aca6878d7571a7d8d5a
kris <kjg9190@hotmail.com>
2019-06-24 17:57:18 +0200

    added server connect help

--- setup_help/Microsoft.PowerShell_profile.ps1
+++ setup_help/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,7 @@
 # Objective: a windows-version ".bashrc" file that can help preload helpful commands.
 
 Set-Alias -Name python -Value python.exe
+function aserver {ssh -i C:\Users\kris\.ssh\id_rsa -p 1122 root@141.69.58.230}
+function bserver {ssh -i C:\Users\kris\.ssh\id_rsa -p 2422 root@141.69.58.230}
+function heimdall {ssh -i C:\Users\kris\.ssh\id_rsa iki@141.69.58.223}
+
