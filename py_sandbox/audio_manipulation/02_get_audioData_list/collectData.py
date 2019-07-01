7275cf5fd391e621e8b6d047a2e034c2d0c9df50
kris <kjg9190@hotmail.com>
2019-07-02 00:18:29 +0200

    fleshing out code. able to work, but having issues with metadata on some files

--- py_sandbox/audio_manipulation/02_get_audioData_list/collectData.py
+++ py_sandbox/audio_manipulation/02_get_audioData_list/collectData.py
@@ -2,15 +2,37 @@
 objective: collect metadata for each song in music folder (recursively)
 '''
 
-import os
+import os, argparse
 import klib as k
-from kaudio import LoadMetaData as LMD
+from kaudio import MetaMP3 as M
 
-music_path = 'D:/Music2/'
+p=argparse.ArgumentParser()
+p.add_argument('--musicDir',type=str,default='.',help='location to analyze')
+p.add_argument('--saveDir',type=str,default='.',help='where to save results')
+p.add_argument('--logName',type=str,default='data.csv',help='name of results file')
+args=p.parse_args()
+# music_path = 'D:/Music2/'
 
-allFiles=k.dir(music_path,rec=True)
+
+
+
+allFiles=k.dir(args.musicDir,rec=True,ext='mp3')
 print('number of files:',len(allFiles))
-print('example:',allFiles[0])
-print('data:')
-LMD(allFiles[0]).list()
-import ipdb; ipdb.set_trace()
+# print('example:',allFiles[0])
+# print('data:')
+
+filename = os.path.join(args.saveDir,args.logName)
+f = open(filename,'w')
+
+tags = M(allFiles[0]).tags
+
+tags2 = ['fname']+tags
+# print(';'.join(tags2))
+f.write(';'.join(tags2)+'\n')
+
+for ifile in allFiles:
+    info2=[M(ifile).getAllData()[itag] for itag in tags]
+    info3=';'.join([ifile]+info2)
+    print('file:',ifile)
+    f.write(info3+'\n')
+print('done')
