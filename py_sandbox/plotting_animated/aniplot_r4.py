d6172d89357ae9ed4a3daf5876a42da68a4a265e
kris <kjg9190@hotmail.com>
2019-09-30 22:33:11 +0200

    opencv works best for plotting

--- py_sandbox/plotting_animated/aniplot_r4.py
+++ py_sandbox/plotting_animated/aniplot_r4.py
@@ -43,10 +43,41 @@ GRN = (0,255,0)
 WHT = (255,255,255)
 BLK = (0,0,0)
 CVFONT = cv2.FONT_HERSHEY_SIMPLEX
+IMW=400
+IMH=300
 def qs(img,title='CLOSE WITH KEYBOARD'):
     cv2.imshow(title,img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def rect2(img, center,dims,angle, color,*kargs):
+    ''' general steps:
+    1. take in parameters
+    2. rotate rectangle centered at origin
+    3. translate to given spot.
+
+     '''
+    xc,yc=center
+    w,h=dims
+    theta = np.radians(angle)
+    c,s=np.cos(theta),np.sin(theta)
+    R=np.array([
+        [c,-s,0],
+        [s,c,0],
+        [0,0,1]]) # 3x3
+    pts=np.array([
+        [-w,h,1],
+        [w,h,1],
+        [w,-h,1],
+        [-w,-h,1],
+        [-w,h,1]   ])/2
+
+    # rotate points
+    pts2=pts@R
+    pts2[:,0]+=xc
+    pts2[:,1]+=yc
+    pts2=pts2[:,:2].reshape((-1,1,2)).astype(int)
+    cv2.polylines(img,[pts2],True,RED)
 
 t0=time.time()
 
@@ -59,46 +90,21 @@ class Timer:
 timer = Timer()
 NOW=timer.now
 
-class Kcirc:
-    def __init__(self,centerRC,radius,freq=1):
-        ''' note: freq in Hz '''
-        self.r = centerRC[0]
-        self.c = centerRC[1]
-        self.radius = radius
-        self.freq = 2*np.pi*freq
-    def update(self):
-        self.r += np.sin(NOW()*self.freq)*self.radius
-        self.c += np.cos(NOW()*self.freq)*self.radius
-    def get(self):
-        self.update()
-        return (self.r.astype(int),self.c.astype(int))
-
-center = Kcirc((400,300),1)
+xc=IMW/2
+yc=IMH/2
+w=50
+h=100
 
 while(True):
-    # calculate center
-    # rest of stuff
     t1=time.time()-t0
     t0=time.time()
-    bkgd = np.ones((600,800,3))*255
-    # noise = np.random.rand(600,800,3) # remember: (r,c) dimensions
+    bkgd = np.ones((IMH,IMW,3))*255 # follows image format
     cv2.putText(bkgd,str(round(t1,3)),(50,30),CVFONT,1,BLU)
     cv2.putText(bkgd,str(round(NOW(),3)),(50,60),CVFONT,1,BLU)
-    # temp=center.get()
-    # print(temp)
-    cv2.circle(bkgd,center.get(),10,BLU)
-    cv2.circle(bkgd,(400,300),10,RED)
-    cv2.rectangle(bkgd,(30,50),(90,60))
-    # alright, try drawing a rectangle with polygon
-
-    # def rect2(img,center,dims,angle)
-
-
-    # pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+    cv2.circle(bkgd,(int(IMW/2),int(IMH/2)),10,GRN)
+    rect2(bkgd,(xc,yc),(w,h),NOW()*180,RED)
+    pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
     pts = pts.reshape((-1,1,2)) # critical for drawing a polygon
-    cv2.polylines(bkgd,[pts],True,RED)
-
-
 
     cv2.imshow('frame',bkgd)
     if cv2.waitKey(1) & 0xFF == ord('q'):
