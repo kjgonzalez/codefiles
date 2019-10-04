3ec3fcdf1472ed90c21799a92f32592a40c81f84
kris <kjg9190@hotmail.com>
2019-10-04 17:55:00 +0200

    put multi-layer-perceptron in a class

--- py_sandbox/book_dataScienceFromScratch/perceptron_etc.py
+++ py_sandbox/book_dataScienceFromScratch/perceptron_etc.py
@@ -9,6 +9,38 @@ NOTE: perceptrons only have single linear operation, can't approximate
 
 import numpy as np
 
+# alright, want to make a class out of this at some point
+
+# class MLP:
+#     ''' new, second attempt at basic NN (see book_OwnNN) '''
+#     def __init__(self):
+#         ''' initialize network '''
+#     def afn(self,x):
+#         ''' activation function: sigmoid '''
+#         return 1.0/(1+np.exp(-x))
+#
+#     def loadWeights(self):
+#         pass
+#     def neuronOut(self,weights,input):
+#         ''' automatically integrate bias into weights '''
+#         wts=np.array(weights)
+#         inp=np.array(input)
+#         return self.afn(  wts[:-1]@inp+wts[-1]  )
+#     def feedforward(self,neural_network,input):
+#         ''' perform one pass forward through network '''
+#         inp=input.copy()
+#         outputs=[]
+#         for ilayer in neural_network:
+#             # for each layer, apply weights
+#             output=np.array([neuronOut(ineuron,inp) for ineuron in ilayer])
+#             outputs.append(output)
+#             inp=output.copy()
+#         return outputs
+
+
+
+
+
 def step_function(x):
     return 1.0 if x>=0 else 0.0 # note: not sure why x=0 == 1...?
 def sigmoid(x):
@@ -46,7 +78,7 @@ def feedforward(neural_network,input):
         outputs.append(output)
         inp=output.copy()
     return outputs
-# eof
+
 xor_net=[
 [[20,20,-30],
 [20,20,-10]],
@@ -57,3 +89,37 @@ print(feedforward(xor_net,[0,0])[-1][0])
 print(feedforward(xor_net,[1,0])[-1][0])
 print(feedforward(xor_net,[0,1])[-1][0])
 print(feedforward(xor_net,[1,1])[-1][0])
+
+
+# now, at some point, want to be able to train this, so will use backpropagation
+
+def sqerror_gradients(neural_net,input,target):
+    '''
+    Given a neural network, an input vector, and a target vector,
+    make a prediction and compute the gradient of the squared error
+    loss with respect to the neuron weights.
+    '''
+
+    # forward pass
+    outputs, hidden_outputs = feed_forward(network, input_vector)
+    # gradients with respect to output neuron pre-activation outputs
+    output_deltas = [output * (1 - output) * (output - target)
+        for output, target in zip(outputs, target_vector)]
+    # gradients with respect to output neuron weights
+    output_grads = [[output_deltas[i] * hidden_output
+        for hidden_output in hidden_outputs + [1]]
+            for i, output_neuron in enumerate(network[-1])]
+    # gradients with respect to hidden neuron pre-activation outputs
+    hidden_deltas = [hidden_output * (1 - hidden_output) *
+        dot(output_deltas, [n[i] for n in network[-1]])
+            for i, hidden_output in enumerate(hidden_outputs)]
+    # gradients with respect to hidden neuron weights
+    hidden_grads = [[hidden_deltas[i] * input for input in input_vector + [1]]
+        for i, hidden_neuron in enumerate(network[0])]
+    return [hidden_grads, output_grads]
+
+
+
+
+
+# eof
