1fc520a283db29f27b1def7817e280dcd602c833
kris <kjg9190@hotmail.com>
2019-10-10 08:33:19 +0200

    rearrangement

---
+++ py_sandbox/ai_practice/reinforcement/reinforce_search.py
@@ -0,0 +1,24 @@
+'''
+datecreated: 191009
+objective: try to make way through maze with reinforcement learning. create an
+    agent that can have certain actions / states and learns to traverse any
+    given maze
+
+
+NOTES:
+* will try to save some time by reusing old code / media from path planning
+    practice, e.g. the maze
+* for now, will also try doing things in matplotlib, only displaying final result
+
+
+source1: https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
+'''
+
+
+
+
+
+
+
+
+# eof
