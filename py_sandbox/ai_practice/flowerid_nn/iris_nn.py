034bbaba089d9b14b3f73e59acdb40ad8701b005
kris <kjg9190@hotmail.com>
2019-11-18 15:25:55 +0100

    Solves #5: nn-based solution for iris dataset is working at 0.933 accuracy

--- py_sandbox/ai_practice/flowerid_nn/iris_nn.py
+++ py_sandbox/ai_practice/flowerid_nn/iris_nn.py
@@ -5,15 +5,17 @@ objective: use neural network to identify flowers in iris dataset based on 4
 
 general steps:
 1. data loading / data prep - done
-2. training                 - waiting
-3. testing for accuracy     - waiting
+2. training                 - done
+3. testing for accuracy     - done
+
 
 NOTES:
+* what if scale from 0.5-8.0 instead of 0-10?
+* KJG191118: several combinations achieve 0.933 accuracy, including:
+    - [4,4,3] @ 1000 epochs
+    - [4,1000,3] @ 4000 epochs
+    - [4,3,3] @ 1000
 * there are 150 samples, so will make a 20% datasplit (30 test samples)
-* estimated architecture:
-    - input layer: 4
-    - hidden layer: 10/50/100 (?)
-    - output layer: 3 (or 1 depending on style)
 * min / max values (keeping rows in order):
     name | sepL | sepW | petL | petW
     min  | 4.3  | 2.0  | 1.0  | 0.1
@@ -27,7 +29,6 @@ NOTES:
 '''
 # INITIALIZATION ===============================================================
 import numpy as np
-np.random.seed(0) # for now, control randomness seed
 from klib import data as da
 import argparse, time
 import os, sys
@@ -40,64 +41,62 @@ def npshuffle(nparr):
     np.random.shuffle(npa2)
     return npa2
 
-# DATALOADING ==================================================================
-
-print("loading data ...")
-dataset=[]
-for irow in open(da.irispath):
-    iraw = irow.strip().split(',') # 1x5 data
-    idat = (0.98/10)*(np.array(iraw[:-1],dtype=float))+0.01 # 0-10 to 0.01-0.99
-    ival = iraw[-1]
-    if('setosa' in ival): ival=0
-    elif('versicolor' in ival): ival=1
-    elif('virginica' in ival): ival=2
-    else: raise Exception('error, species not recognized')
-    ians = np.zeros(3)+0.01
-    ians[ival] = 0.99
-    dataset.append([idat,ians])
-# at this point, have a 150x5 array of data
-dataset=npshuffle(dataset) # shuffle data
-
-print("data loaded")
-ntrain = 120
-ds_train=dataset[:ntrain]
-ds_test =dataset[ntrain:]
-
-print('training:')
-for i in ds_train: print(i)
-print('testing')
-for i in ds_test: print(i)
-
-# TRAINING PHASE ===============================================================
-print('initializing network')
-layers=[4,20,3]
-LR=0.01 # this value is arbitrary, remember
-nn=NeuralNetwork(layers,LR)
-
-# presumably, training would happen here
-
-
-# TESTING PHASE ================================================================
-scorecard=[]
-for idat in ds_test:
-    answer=np.argmax(idat[1])
-    pred=np.argmax(nn.query(idat[0]))
-    print(answer,'|',pred)
-    scorecard+=[1] if(answer==pred) else [0]
-print(scorecard)
-print('performance:',sum(scorecard)/len(scorecard))
-
-
-
 if(__name__=='__main__'):
     p=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('--random',default=False,action='store_true',help='Disable randomness control')
+    p.add_argument('--random',default=False,action='store_true',help='Enable randomness')
+    p.add_argument('--epochs',default=1000,type=int,help='number of epochs to train')
+    p.add_argument('--lr',type=float,default=0.1,help='Learning rate')
+    p.add_argument('--layers',type=str,default='4-4-3',help='nn layer composition')
     # ideas: n epochs? split size?
     args=p.parse_args()
     print('done')
 
+    if(not args.random):
+        np.random.seed(0) # for now, control randomness seed
+        print('seed controlled')
+    nepochs = args.epochs
 
+    layers=[int(i) for i in args.layers.split('-')]
+    # DATALOADING ==================================================================
+    print("loading data...")
+    dataset=[]
+    for irow in open(da.irispath):
+        iraw = irow.strip().split(',') # 1x5 data
+        dmin= 0.0# domain minimum
+        dmax= 10.0# domain maximum
+        idat = (0.98/(dmax-dmin))*(np.array(iraw[:-1],dtype=float)-dmin)+0.01 # 0-10 to 0.01-0.99
+        ival = iraw[-1]
+        if('setosa' in ival): ival=0
+        elif('versicolor' in ival): ival=1
+        elif('virginica' in ival): ival=2
+        else: raise Exception('error, species not recognized')
+        ians = np.zeros(3)+0.01
+        ians[ival] = 0.99
+        dataset.append([idat,ians])
+    # at this point, have a 150x5 array of data
+    dataset=npshuffle(dataset) # shuffle data
 
+    ntrain = 120
+    ds_train=dataset[:ntrain]
+    ds_test =dataset[ntrain:]
+
+    # TRAINING PHASE ===============================================================
+    print('initializing network...')
+    LR=0.1 # this value is arbitrary, remember
+    nn=NeuralNetwork(layers,LR)
+    print('nepochs:',nepochs,'|  layers:',layers)
+    # presumably, training would happen here
+    nn.train_full(ds_train,nepochs)
+
+    # TESTING PHASE ================================================================
+    scorecard=[]
+    for idat in ds_test:
+        answer=np.argmax(idat[1])
+        pred=np.argmax(nn.query(idat[0]))
+        # print(answer,'|',pred)
+        scorecard+=[1] if(answer==pred) else [0]
+    # print(scorecard)
+    print('performance:',sum(scorecard)/len(scorecard))
 
 
 # eof
