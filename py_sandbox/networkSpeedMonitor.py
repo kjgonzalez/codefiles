c64fead68d4d629e70e96fa0805fa1985fb4bc4b
kris <kjg9190@hotmail.com>
2020-01-08 22:57:43 +0100

    updating

--- py_sandbox/networkSpeedMonitor.py
+++ py_sandbox/networkSpeedMonitor.py
@@ -3,23 +3,18 @@ Author: Kris Gonzalez
 Date Created: 180509
 Objective: make simple python script to monitor network download / upload speed
 
-note: in the future, may care about which network
-    connection am looking at. for now, will stick to
-    ethernet.
+NOTES:
+* download / upload values are in KB/s (kilobytes/second)
+* cycle time isn't exactly the value given, closer to about 2% longer
+* possible network connections at sample house:
+    0 : Ethernet
+    1 : Ethernet 3
+    2 : Local Area Connection* 1
+    3 : Local Area Connection* 2
+    4 : Ethernet 2
+    5 : Wi-Fi
+    6 : Loopback Pseudo-Interface 1
 
-possible network connections at sample house:
-0 : Ethernet
-1 : Ethernet 3
-2 : Local Area Connection* 1
-3 : Local Area Connection* 2
-4 : Ethernet 2
-5 : Wi-Fi
-6 : Loopback Pseudo-Interface 1
-
-
-
-# ISSUES:
-please check whether data is in BYTES or BITS
 '''
 
 import psutil,time, argparse
@@ -31,32 +26,32 @@ def getData(source='Ethernet'):
     return np.array([time.time(),data.bytes_recv,data.bytes_sent],dtype=float)
     # return(float(time.time()),float(data.bytes_recv),float(data.bytes_sent))
 
-def calculateRate(dt,source):
+def calculateRates(dt,sourceList):
     ''' given a waiting time (dt [s]), calculate download / upload rates (in Kbps) '''
-    dat0 = getData(source)
+    dat0=[]
+    dat1=[]
+    res=[]
+    for isrc in sourceList:
+        dat0 += [getData(isrc)]
     time.sleep(dt)
-    dat1 = getData(source)
+    for i,isrc in enumerate(sourceList):
+        temp = getData(isrc)-dat0[i]
+        # iDownUp=(temp[1:]/temp[0]/1024.).round(2)
+        iDownUp=(temp[1:]/1024./temp[0]).round(2)
+        res.append([*iDownUp,isrc])
+    res=np.array(res,dtype=object)
+    return res
 
-    res=dat1-dat0
-    rateDn=res[1]/res[0]
-    rateUp=res[2]/res[0]
-    return (res[1:]/res[0]/1024.).round(2)
-
-def testSources(dt,sourcesList):
-    ''' for each given source, return upload / download rate (in Kbps) '''
-
-    for isrc in sourcesList:
-        pass
-    pass
-
-ans=    calculateRate(1,'Wi-Fi')
-import ipdb; ipdb.set_trace()
+def calculateOne(dt,source):
+    ''' determine data of single source '''
+    temp=calculateRates(dt,[source])[0,:2]
+    return list(temp)
 
 if(__name__=='__main__'):
     p=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('--list',default=False,action='store_true',help='lists available networks, then exits')
     p.add_argument('--src',type=str,default='auto',help='internet source to measure. "auto" = automatically detect active network')
-    p.add_argument('--p',default=1.0,type=int,help='cycle period')
+    p.add_argument('--p',default=1.0,type=float,help='cycle period')
     p.add_argument('--n',default=10,type=int,help='number of cycles to run. -1 means infinite.')
     args=p.parse_args()
     src=args.src
@@ -68,15 +63,16 @@ if(__name__=='__main__'):
         # list available sources, then exit
         print('available connections (use either number or name as source):')
         [print(j,':',i) for j,i in enumerate(items)]
+
         print('\nExiting...')
         exit()
 
     if(args.src=='auto'):
-        print('auto-determining source')
-
-        exit()
-# A: want to be able to auto-determine source
-# B: wanna be able to use number OR source name
+        print('auto-determining source...')
+        # find max(2norm(allSources))
+        res = calculateRates(1,items)
+        ind=np.argmax([np.linalg.norm(i[:2]) for i in res])
+        src=items[ind]
 
     # if user gave number, convert to string
     if(src.isdigit()):
@@ -85,13 +81,15 @@ if(__name__=='__main__'):
     print('period:',s)
     print('Ntimes:',n)
     print('')
-if(n==-1):
-    # run infinite times
-    while(True):
-        print(calculateRate(s,src))
-if(n>0):
-    # run n times
-    for i in range(n):
-        print(calculateRate(s,src))
-else:
-    raise Error('invalid value of n used:',n)
+    print('Down  Up')
+
+    if(n==-1):
+        # run infinite times
+        while(True):
+            print( *calculateOne(s,src) ) # single source
+    if(n>0):
+        # run n times
+        for i in range(n):
+            print( *calculateOne(s,src) ) # single source
+    else:
+        raise Error('invalid value of n used:',n)
