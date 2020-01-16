4721786aee7d0effafb325d9838f1370159d1c14
kris <kjg9190@hotmail.com>
2020-01-16 11:26:19 +0100

    #6 able to publish ros image messages on local

--- py_sandbox/rosremote/rpi_node.py
+++ py_sandbox/rosremote/rpi_node.py
@@ -11,6 +11,11 @@ import cv2
 import numpy as np
 import sys,argparse,time,os
 import klib
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
 st = klib.Stamper()
 now = lambda :st.now().replace(':','') # dont want colons in filename
 
@@ -25,12 +30,42 @@ def takephoto(src,savepath='.'):
     cv2.imwrite(fpath,frame)
     print('photo saved:',fpath)
 
+def temp_talker():
+    ''' keeping only as debugging tool '''
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1) # default:10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
 
+def imgout():
+    ''' publish images (aka video stream). can be checked with rviz '''
+    pub = rospy.Publisher('campub', Image, queue_size=10) # topic here
+    rospy.init_node('campub', anonymous=True) # publisher here
+    rate = rospy.Rate(1) # default:10hz
+    bridge=CvBridge()
+    while not rospy.is_shutdown():
+        ret,frame = cap.read()
+        avgpixel = frame.mean(0).mean(0).astype(int)
+        print('avg value (BGR):',avgpixel)
+        rosimg=bridge.cv2_to_imgmsg(frame) # ,"bgr8"
+        try:
+            pub.publish(rosimg)
+        except CvBridgeError as e:
+            print(e)
+        #rate.sleep() # may need to use when debugging
 
 if(__name__=='__main__'):
     p=argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('--src',default=0,type=int,help='camera source')
     args=p.parse_args()
-
-
-    takephoto(args.src)
+    cap = cv2.VideoCapture(args.src)
+    
+    try:
+        imgout() # this function goes into internal while loop
+    except rospy.ROSInterruptException:
+        pass
+    cap.release()
