ef9a2662c7d9491c7c2321122b74a5e65290b89e
kris <kjg9190@hotmail.com>
2020-02-03 20:59:23 +0100

    different ways to pass arguments to a function

--- cpp_sandbox/pass_by_reference_etc/pass_by_reference_etc.cpp
+++ cpp_sandbox/pass_by_reference_etc/pass_by_reference_etc.cpp
@@ -4,6 +4,7 @@ will demonstrate examples of passing by various methods, as well as explaining
     * pass by value
     * pass by reference
     * pass by pointer
+    * pass by definition (as argument)
 
 
 */
@@ -16,38 +17,41 @@ class Circle{
 public:
     // member variables
     float r;
-
+    
     // member functions
     Circle(float r_){r=r_;}
-    float area(){return pi*r*r;}
+    float area() const {return pi*r*r;} // REQUIRED "const" if you need to pass as const object
+    // void addOne() const {r+=1;} // NOT allowed: const function with reassignment
 };
 
-float byValue(Circle obj, Circle obj2){ // pass args by value
+float byValue(Circle obj){ // pass args by value
     obj.r +=1;
     return obj.area();
 }
 
-float byRef(Circle &obj,Circle &obj2){ // pass args by reference
+float byRef(Circle &obj){ // pass args by reference
     obj.r +=1;
     return obj.area();
 }
 
-float byPointer(Circle *obj, Circle *obj2){ // pass args by pointer
+float byPointer(Circle *obj){ // pass args by pointer
     obj->r +=1;
-    obj2 = obj;
-    cout << "obj2 radius: " << obj2->r << endl;
     return obj->area();
 }
 
+float byDefine(const Circle &obj){
+    // obj.r +=1; // not allowed if "const"
+    // return obj.r*obj.r*pi;
+    return obj.area();
+}
 
 int main(){
     Circle c(1);
     printf(" r: %f \n area: %f \n", c.r,c.area() );
     cout << "-------------------------" << endl;
-    printf("area, pass by val: %f\n",byValue(c,c)); // makes a copy of argument
-    printf("area, pass by ref: %f\n",byRef(c, c ));   // modifes reference directly, can't be NULL or changed
-    printf("area, pass by poi: %f\n",byPointer(&c, NULL)); // modifies pointer object, may be NULL and changed
-
-
+    printf("pass by val: %f\n",byValue(c)); // makes a copy of argument
+    printf("pass by ref: %f\n",byRef(c));   // modifes reference directly, can't be NULL or changed
+    printf("pass by poi: %f\n",byPointer(&c)); // modifies pointer object, may be NULL and changed
+    printf("pass by def: %f\n",byDefine(Circle(4)) ); //declare a CONSTANT object as argument
     return 0;
 }
