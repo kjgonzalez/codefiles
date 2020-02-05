712eac966ea9069f2d6ceb16274d1a69114d200e
kris <kjg9190@hotmail.com>
2020-02-05 17:48:40 +0100

    gonna be showcasing various class properties in one set of code

---
+++ cpp_sandbox/classes_help/classes_help.cpp
@@ -0,0 +1,17 @@
+/*
+want to cover basic stuff about classes, but put EVERYTHING you can in here.
+
+NOTE:
+to compile this:
+g++ *.cpp -std=c++11 // don't need cmake, std11 NOT required...
+
+STAT | DESCRIPTION
+???? | simple class
+???? | inheritance
+???? | composition
+???? | private / protected / public
+???? | virtual function
+
+*/
+
+#include <iostream>
