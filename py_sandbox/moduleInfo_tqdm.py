8813f6f101b6ef42fb5c50f1047428cbbfb5ae88
Kris (uic68228) <Kristian.Gonzales@continental-corporation.com>
2020-03-20 15:15:02 +0100

    tqdm info, updating notes

--- py_sandbox/moduleInfo_tqdm.py
+++ py_sandbox/moduleInfo_tqdm.py
@@ -17,12 +17,14 @@ print('done')
 # the above can also be accomplished with trange, which replaces tqdm(range(...),...)
 for i in trange(3,desc='trange'):
     sleep(0.01)
+print()
 
 # apparently nested progress bars are normal
-for i in tqdm(range(2),desc='iloop'):
-    for j in tqdm(range(2),desc='jloop'):
-        for k in tqdm(range(2),desc='kloop'):
-            sleep(0.1)
+for i in tqdm(range(3),desc='iloop'):
+    for j in tqdm(range(3),desc='jloop'):
+        for k in tqdm(range(3),desc='kloop',leave=False):
+            sleep(0.5)
+print()
 
 # NOT recommended to use "print" while in a tqdm loop, breaks up the behavior
 # instead, use tqdm.write
@@ -30,3 +32,4 @@ for i in trange(5):
     # print('in iter',i)
     tqdm.write('in iter {}'.format(i))
     sleep(0.2)
+
