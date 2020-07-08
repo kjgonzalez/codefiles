a68b35f10abcc37328c973a5337faf3532feb823
Kris (uic68228) <Kristian.Gonzales@continental-corporation.com>
2020-07-08 13:56:04 +0200

    removing import tutorial, doesnt seem to be helpful

--- py_sandbox/remote_module_import/parent_folder/subfolder/module2.py
+++
@@ -1,7 +0,0 @@
-'''
-This module located in a subfolder from importing module, and may require
-    different importing technique from one in a parent / remote folder
-'''
-
-def pyt2(arr):
-    return (sum([i**2 for i in arr]))**0.5
