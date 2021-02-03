71e39ba68ad5e3d436eb20ba58bfc3474f64002d
kjg <kjg9190@hotmail.com>
2021-02-03 22:14:14 +0100

    basic notes on r

---
+++ r_sandbox/classes_and_objects.r
@@ -0,0 +1,14 @@
+# # basics
+# 
+# studentBio <- list(
+#   studentName = "Harry",
+#   studentAge = 19,
+#   studentContact = "London"
+# )
+# class(studentBio <- "StudentInfo")
+# 
+# studentBio
+
+
+print("test")
+
