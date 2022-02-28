55ebc6811b8f86d23ae005b29fae4aaf983f6979
kjgonzalez <kjg9190@hotmail.com>
2022-02-28 23:32:46 +0100

    module info

--- py_sandbox/moduleInfo_StdLib.py
+++ py_sandbox/moduleInfo_StdLib.py
@@ -33,6 +33,43 @@ import os
 x = os.popen('echo hello').read()
 print(x)
 
+# 03 Bytes<>Ints<>Hex =============================================================
+'''
+want to understand how bytes are manipulated & converted in python
+DO NOT USE "hex()"! THIS ONLY RETURNS A NORMAL STRING OF WHAT A DECIMAL VALUE LOOKS LIKE AS A HEX
+'''
+
+# alphanumeric data
+bytestr = b'this' # print() => b'this' # note: string of bytes
+normstr = bytestr.decode() # print() => 'this' # normal string
+bytestr2 = normstr.encode() # back to a bytestr
+print(bytestr)
+
+# note that mixing encodings can lead to incorrect-looking data
+print(
+    (chr(0).encode('utf-16')+'this'.encode()).decode('utf-16')
+)
+
+# numeric data: switch from between: decimal <=> hex <=> binary <=> decimal
+# ex: 165 = 1010 0101 = 0xA5. want to pull out exact values from each place
+valint=165
+valhex=b'\xa5' # 165
+valbin='0b10100101'
+
+# how to swap between each:
+print('as int :',valint)
+print('int2hex:',valint.to_bytes(1,'little'))
+print('int2bin:',bin(valint))
+print('---')
+print('as hex :',valhex)
+print('hex2int:',int.from_bytes(valhex,'little'))
+print('hex2bin:',bin(int.from_bytes(valhex,'little')))
+print('---')
+print('as bin :',valbin)
+print('bin2int:',int(valbin,2))
+print('bin2hex:',int(valbin,2).to_bytes(1,'little'))
+
+# eof
 
 
 
--- py_sandbox/moduleInfo_datetime.py
+++ py_sandbox/moduleInfo_datetime.py
@@ -9,3 +9,10 @@ print('date:',dt.date(2020,10,9))
 print('time:',dt.time(23,15,11))
 print('now:',dt2.now())
 print('yesterday:',dt2.now()-dt.timedelta(1))
+res = dt.date(2021,10,3).strftime('%Y%m%d')
+print('date2:',res)
+
+res = dt.date(2020,10,9)-dt.date(2019,9,9)
+
+print('diff:',res)
+print('from string:',dt2.strptime('20201009','%Y%m%d'))
