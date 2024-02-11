88961f867452eba445d2d36c765a7a4cb873c248
kjg <k@j.g>
2024-02-11 19:21:52 +0100

    update

--- setup_help/Microsoft.PowerShell_profile.ps1
+++ setup_help/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,19 @@
 # Created: 190507
 # Objective: a windows-version ".bashrc" file that can help preload helpful commands.
 
-Set-Alias -Name python -Value python.exe
-function aserver {ssh -i C:\Users\kris\.ssh\id_rsa -p 1122 root@141.69.58.230}
-function bserver {ssh -i C:\Users\kris\.ssh\id_rsa -p 2422 root@141.69.58.230}
-function heimdall {ssh -i C:\Users\kris\.ssh\id_rsa iki@141.69.58.223}
-
-function ipython {ipython.exe --nosep}
+# function ipython {ipython.exe --nosep} # not necessary
+# first time setup of ipython: 
+# 1. (go into relevant venv as needed)
+# 2. >> ipython profile create
+# 3. open relevant file, e.g. "~\.ipython\profile_default\ipython_config.py"
+# 4. change following fields: 
+#   a. c.TerminalInteractiveShell.highlighting_style = "one-dark" # good color
+#   b. c.InteractiveShell.separate_in = '' # same as --nosep
 
 Set-Alias -Name sumatra -Value "C:\Program Files\SumatraPDF\SumatraPDF.exe"
 Set-Alias -Name npp -Value "C:\Program Files (x86)\Notepad++\notepad++.exe"
 Set-Alias -Name foxit -Value "D:\Programs2\FoxitReader\FoxitReader.exe"
+Set-Alias -Name activate -Value "C:\Users\kjg\vemain\Scripts\Activate.ps1"
 
 function cpprun{
     echo "SYSTEM: building..."
