b240daf1de2efb78f87da8a91601be980b6755fc
kphone <kjg9190@hotmail.com>
2024-03-14 08:13:42 +0100

    misc

---
+++ c_sandbox/structs_etc/main.c
@@ -0,0 +1,26 @@
+/*
+get a slightly more intimate understanding of structs and other things
+
+*/
+
+#include "stdlib.h"
+#include <time.h>
+
+
+int main(){
+    printf("hello world\n");
+    printf("current time: %d",2);
+    printf("\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+// eof
