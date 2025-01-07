2ef0406ad77c1ca3acd3ff3b1f3c426d8a7ca16c
kjg <k@j.g>
2025-01-08 00:33:24 +0100

    testing out go

---
+++ go_sandbox/hello_go/hello.go
@@ -0,0 +1,25 @@
+/*
+block comment.
+k250108: first attempt at using go. statically
+  typed compiled lanaguage, seems easy to use
+
+main questions:
+  matrix package?
+  plotting?
+  gui?
+  multithreading / multiprocessing?
+  web capabilities (+mqtt?)
+
+*/
+
+package main // line comment: 'package' = collection of one/multiple go files
+
+import "fmt" // specific features of a particular package
+
+func main() {
+    fmt.Println("hey world")
+    
+    
+
+
+}
