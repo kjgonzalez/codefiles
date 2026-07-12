579a586ed983eea1b4910fd8fc0a360d7d644380
kphone <kjg@mail>
2026-07-12 22:31:51 +0200

    argparse

--- cpp_sandbox/lib_kargparse/main.cpp
+++ cpp_sandbox/lib_kargparse/main.cpp
@@ -63,7 +63,7 @@ class KArgParser
 
     string _desc;
     Argitem _args[100];
-    std::map<std::string,int> _argmap;
+    std::map<std::string,int> _argmap; // argname, index in _args array
     //typedef _argmap::iterator _argmapit;
     int _argslen;
     int _nreq;
@@ -94,6 +94,7 @@ public:
         argi.desc = std::string(desc);
         argi.atype = atype;
         _args[_argslen] = argi;
+        _argmap[std::string(name)]=_argslen;
         _argslen+=1;
         _nreq+=1;
     }
@@ -108,12 +109,21 @@ public:
             printf("  %s | %s\n",_args[i].name.c_str(),_args[i].desc.c_str());
         }
 
+        // show argitem pairs
+        //printf("_argmap len: %d\n",_argmap.size());
+        //return -1;
+        //for(auto ipair : _argmap){
+        //    
+        //}
+
+
+
+        // perform parsing
         if(strcmp(av[1],"--help")==0){
             printhelp(av[0]);
             return 0;
         }
 
-        // perform parsing
         int argind = 0;
         for(argind=0;argind<_nreq;argind++) {
             _args[argind].setval(av[argind+1]);
@@ -123,6 +133,7 @@ public:
     }
     void get(const char* name,unsigned long &v)
     {
+        printf("get-uint\n");
         int ind = _argmap.find(name)->second;
         printf("%s ind: %d\n",name,ind);
         assert(_args[ind].atype==atype_uint);
@@ -132,8 +143,13 @@ public:
 
     void get(const char* name,double &v)
     {
+        printf("get-double, %s\n",name);
+        auto iname = _argmap.find(name)->first;
+        std::cout << "test: " << iname << std::endl;
+        //printf("here\n");
         int ind = _argmap.find(name)->second;
-        printf("%s ind: %d\n",name,ind);
+        printf("here2\n");
+        printf("%s ind: %s, %d\n",name,iname.c_str(), ind);
         assert(_args[ind].atype==atype_double);
         v = _args[ind].vald;
         return;
@@ -165,7 +181,7 @@ int main(int argc, char** argv)
     ap.get("intensity", intensity);
     ap.get("fraction", fraction);
     printf("main scope, intensity=%lu\n",intensity);
-    printf("main scope, fraction=%d\n",fraction);
+    printf("main scope, fraction=%f\n",fraction);
     printf("done\n");
     
     return 0;
