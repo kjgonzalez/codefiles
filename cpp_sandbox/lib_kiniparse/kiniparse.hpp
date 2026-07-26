ee42925d16083840d03401812b1eccb8e27e31a7
kjg <k@j>
2026-07-27 01:24:38 +0200

    added list parsing

--- cpp_sandbox/lib_kiniparse/kiniparse.hpp
+++ cpp_sandbox/lib_kiniparse/kiniparse.hpp
@@ -1,141 +1,181 @@
 /*
-kris' ini parser - a simple drop-in way to read and use INI files
+Kris' Ini File Parser header-only library- a simple drop-in way to read and use INI files
 
 * ';' and '#' are comments
 * comments must be on own line, no in-line comments
 * whitespace is stripped everywhere except within quotes
 * sections are ignored
-* allowed value types: string, int64, double
-
-todo: 
-* allow single-type array (as vector)
+* allowed value types: string, int64, double, list of previous
 
 */
-#include <fstream>
-#include <string> // read from file into buffer
-#include <sstream> // to check if number
-#include <map> // final object holding all key-value pairs
-//#include <vector> - todo, add
-#include <assert.h>
+#include <assert.h> // minor asserts
+#include <fstream>  // open file
+#include <string>   // read from file into buffer
+#include <sstream>  // to check if number
+#include <map>      // final object holding all key-value pairs
+#include <vector>   // parse lists
 
-namespace kip
+class IniParse
 {
-    class IniParse
+    void trim_for_ini(std::string& s)
     {
-        void trim_for_ini(std::string& s)
+        int i = 0;
+        bool inquotes = false; // don't remove whitespace from within quotes
+        while (i < s.length())
         {
-            int i = 0;
-            bool inquotes = false; // don't remove whitespace from within quotes
-            while (i < s.length())
-            {
-                if (s[i] == '"') inquotes = !inquotes; // todo
-                if (s[i] == ' ' && !inquotes) s = s.replace(s.begin() + i, s.begin() + i + 1, "");
-                else i++;
-            }
+            if (s[i] == '"') inquotes = !inquotes; // todo
+            if (s[i] == ' ' && !inquotes) s = s.replace(s.begin() + i, s.begin() + i + 1, "");
+            else i++;
         }
+    }
 
-        class Myval
+    class Myval
+    {
+        bool is_numeric(std::string raw)
         {
-            bool is_numeric(std::string raw)
-            {
-                std::stringstream ss;
-                ss << raw;
-                double f = 0;
-                ss >> f;
-                if (ss.good() || (f == 0 && raw[0] != '0')) return false;
-                return true;
-            }
-        public:
-            enum valtype { vtype_none, vtype_int, vtype_dbl, vtype_str };
-            valtype vtype;
-            int64_t vi;
-            double vd;
-            std::string vs;
-            Myval() :vtype(vtype_none), vi(-1), vd(-1.1), vs("") {}
-
-            void parse_str(std::string rawval, bool v = false)
-            {
-
-                if (!is_numeric(rawval)) {
-                    assert(rawval[0] == '"');
-                    assert(rawval[rawval.size() - 1] == '"');
-
-                    vtype = vtype_str;
-                    this->vs = rawval.substr(1, rawval.size() - 2);
-
-                    if (v) printf("'%s' set as string\n", this->vs.c_str());
-                }
-                else if (rawval.find('.') == -1) {
-                    vtype = vtype_int;
-                    this->vi = std::stoi(rawval);
-                    if (v) printf("'%s' set as int\n", rawval.c_str());
-                }
-                else {
-                    vtype = vtype_dbl;
-                    this->vd = std::stod(rawval);
-                    if (v) printf("'%s' set as double\n", rawval.c_str());
-                }
-            }
-            template<typename T>
-            T get()
-            {
-
-                if constexpr (std::is_same_v<T, double>)
-                    return vd; // type double
-                if constexpr (std::is_same_v <T, int64_t>)
-                    return vi; // type int64
-                if constexpr (std::is_same_v <T, std::string>)
-                    return vs; // type string
-                printf("ERR invalid value dtype\n");
-                throw 1;
-            }
-        };
-
-        std::map<std::string, Myval> d;
-        bool is_parsed = false;
-        bool verbose = false;
-
-
+            std::stringstream ss;
+            ss << raw;
+            double f = 0;
+            ss >> f;
+            if (ss.good() || (f == 0 && raw[0] != '0')) return false;
+            return true;
+        }
     public:
-        IniParse(bool verbose_ = false) :is_parsed(false), verbose(verbose_) {}
+        enum valtype { vtype_none, vtype_int, vtype_dbl, vtype_str,vtype_listint, vtype_listdbl, vtype_liststr };
+        enum valtype vtype;
+        int64_t vi;
+        double vd;
+        std::string vs;
+        std::vector<int64_t> veci;
+        std::vector<double> vecd;
+        std::vector<std::string> vecs;
+        Myval() :vtype(vtype_none), vi(-1), vd(-1.1), vs("") {}
 
-        //load and parse INI config file
-        int load(const char* path)
+        enum valtype parse_type(std::string& s, bool v = false)
         {
-            std::ifstream fin;
-            std::string buf;
-            fin.open(path);
-            while (!fin.eof()) {
-                std::getline(fin, buf); // note: strips out end of line (\n or \r\n)
-                trim_for_ini(buf);
-                if (buf.size() == 0) continue;
-                else if (buf[0] == ';' || buf[0] == '#') continue;
-                else if (buf[0] == '[') continue; // skip sections for now
-                else if (buf.find('=') == -1) continue; // can't be eval'd for key-val pair
-                //else if (buf.find('[') != -1) continue; // skip lists for now
-                // otherwise, should parse
-
-                size_t split = buf.find('=');
-                auto keyraw = buf.substr(0, split);
-                auto valraw = buf.substr(split + 1);
-                if (valraw.find('[') != -1) continue; // skip lists for now
-
-                d[keyraw] = Myval();
-                d[keyraw].parse_str(valraw, verbose);
-                if (verbose) printf("PARSED: %s, %s\n", keyraw.c_str(), valraw.c_str());
-
-            }
-            fin.close();
-            if (verbose) printf("parsing complete\n");
-            return 0;
+            if (s[0] == '[' && s[s.size() - 1] == ']' && s.find('"') != -1) return vtype_liststr;
+            else if (s[0] == '[' && s[s.size() - 1] == ']' && s.find('.') != -1) return vtype_listdbl;
+            else if (s[0] == '[' && s[s.size() - 1] == ']') return vtype_listint;
+            else if (!is_numeric(s) && s[0] == '"' && s[s.size() - 1] == '"') return vtype_str;
+            else if (is_numeric(s) && s.find('.') != -1) return vtype_dbl;
+            else if (is_numeric(s)) return vtype_int;
+            return vtype_none;
         }
+
+        void parse_list(std::string& s, bool v = false)
+        {
+            size_t p0 = 0;
+            size_t p1 = 0;
+            // determine type of first, set for all
+            while (p1 < s.size()) {
+                p1 = s.find(',', p0);
+                if (vtype == vtype_listint) {
+                    veci.push_back( std::stoi(s.substr(p0,p1-p0)) );
+
+                }
+                else if (vtype == vtype_listdbl) {
+                    vecd.push_back( std::stod(s.substr(p0, p1-p0)) );
+                }
+                else { // vtype==vtype_liststr
+                    if (p1 == -1) p1 = s.size(); // deal with quotation marks & string length
+                    vecs.push_back( s.substr(p0+1,p1-p0-2) );
+                }
+                p0 = p1 + 1;
+            }//while
+        }//parse_list
+
+        void parse_str(std::string& rawval, bool v = false)
+        {
+            vtype = parse_type(rawval, v);
+            switch (vtype) {
+            case(vtype_int):
+                this->vi = std::stoi(rawval);
+                if (v) printf("'%s' set as int\n", rawval.c_str());
+                break;
+            case(vtype_dbl):
+                this->vd = std::stod(rawval);
+                if (v) printf("'%s' set as double\n", rawval.c_str());
+                break;
+            case(vtype_str):
+                this->vs = rawval.substr(1, rawval.size() - 2);
+                if (v) printf("'%s' set as string\n", this->vs.c_str());
+                break;
+            case(vtype_listint):
+                parse_list(rawval.substr(1,rawval.size()-2), v);
+                if (v) printf("'%s' set as int list\n", rawval.c_str());
+                break;
+            case(vtype_listdbl):
+                parse_list(rawval.substr(1, rawval.size() - 2), v);
+                if (v) printf("'%s' set as double list\n", rawval.c_str());
+                break;
+            case(vtype_liststr):
+                parse_list(rawval.substr(1, rawval.size() - 2), v);
+                if (v) printf("'%s' set as string list\n", rawval.c_str());
+                break;
+            default:
+                printf("INVALID TYPE DETECTED\n");
+                assert(vtype != vtype_none);
+            }//case
+        }//parse_str
         template<typename T>
-        T get(std::string key) { return d[key].get<T>(); }
+        T get()
+        {
+            if constexpr (std::is_same_v <T, int64_t>)
+                return vi;
+            if constexpr (std::is_same_v<T, double>)
+                return vd;
+            if constexpr (std::is_same_v <T, std::string>)
+                return vs;
+            if constexpr (std::is_same_v <T, std::vector<int64_t>>)
+                return veci;
+            if constexpr (std::is_same_v < T, std::vector<double>>)
+                return vecd;
+            if constexpr (std::is_same_v < T, std::vector<std::string>>)
+                return vecs;
+            printf("ERR invalid value dtype\n");
+            throw 1;
+        }
     };
 
+    std::map<std::string, Myval> d;
+    bool is_parsed = false;
+    bool verbose = false;
 
-}
 
+public:
+    IniParse(bool verbose_ = false) :is_parsed(false), verbose(verbose_) {}
 
+    //load and parse INI config file
+    int load(const char* path)
+    {
+        std::ifstream fin;
+        std::string buf;
+        fin.open(path);
+        while (!fin.eof()) {
+            std::getline(fin, buf); // note: strips out end of line (\n or \r\n)
+            trim_for_ini(buf);
+            if (buf.size() == 0) continue;
+            else if (buf[0] == ';' || buf[0] == '#') continue;
+            else if (buf[0] == '[') continue; // skip sections for now
+            else if (buf.find('=') == -1) continue; // can't be evaluated for key-val pair
+            //else if (buf.find('[') != -1) continue; // skip lists for now
+            // otherwise, should parse
 
+            size_t split = buf.find('=');
+            auto keyraw = buf.substr(0, split);
+            auto valraw = buf.substr(split + 1);
+            //if (valraw.find('[') != -1) continue; // skip lists for now
+
+            d[keyraw] = Myval();
+            d[keyraw].parse_str(valraw, verbose);
+            if (verbose) printf("PARSED: %s, %s\n", keyraw.c_str(), valraw.c_str());
+
+        }
+        fin.close();
+        if (verbose) printf("parsing complete\n");
+        return 0;
+    }
+    template<typename T>
+    T get(std::string key) { return d[key].get<T>(); }
+};
 
--- cpp_sandbox/lib_kiniparse/test_kiniparse.cpp
+++ cpp_sandbox/lib_kiniparse/test_kiniparse.cpp
@@ -9,8 +9,8 @@ simple demo of how ini parse works
 int main()
 {
     printf("--- Kris' Ini Parse ---\n");
-    std::string fpath = std::string(BASEPATH)+"/test1.ini";
-    kip::IniParse ini(false);
+    std::string fpath = std::string(BASEPATH)+"/test.ini";
+    IniParse ini(false);
     ini.load(fpath.c_str());
 
     printf("---\n");
@@ -18,7 +18,9 @@ int main()
     std::cout << "mystr: " << ini.get<std::string>("mystr") << std::endl;
     std::cout << "myint: " << ini.get<int64_t>("myint") << std::endl;
     std::cout << "myfloat: " << ini.get<double>("myfloat") << std::endl;
-
+    std::cout << "listint: " << ini.get <std::vector<int64_t>>("list_int").size() << std::endl;
+    std::cout << "listdbl: " << ini.get <std::vector<double>>("list_dbl").size() << std::endl;
+    std::cout << "liststr: " << ini.get <std::vector<std::string>>("list_str").size() << std::endl;
 
     printf("exiting...\n");
     return 0;
